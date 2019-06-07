67f9c9ecff73aae835f4bc67a018a3d1b99d91aa
Anthony Dalfonso <anthony@anthonydalfonso.com>
2019-06-06 23:25:24 -0400

    Refactor: convert Display to typescript

--- src/js/Display.ts
+++ src/js/Display.ts
@@ -1,16 +1,23 @@
 import { handler } from './app';
+import Game from './Game';
+import Vector from './Vector';
 
 class Display {
-    constructor(canvas, game) {
+
+    protected game: any;
+    protected ctx: CanvasRenderingContext2D;
+    protected buffer: CanvasRenderingContext2D;
+
+    constructor(canvas: HTMLCanvasElement, game: Game) {
         this.game = game;
         this.ctx = canvas.getContext('2d');
         this.buffer = document.createElement('canvas').getContext('2d');
-        this.triggerResize(this.game.width, this.game.height);
+        this.triggerResize();
 
         handler.register(this);
     }
 
-    draw(offset) {
+    draw(offset: Vector) {
         this.buffer.clearRect(0, 0, this.game.width, this.game.height);
         this.buffer.save();
         this.buffer.translate(offset.x, offset.y);
@@ -59,9 +66,9 @@ class Display {
 
     triggerResize() {
         if (window.innerWidth < this.game.resolution.x) {
-            let ratio = this.game.height / this.game.width;
-            let width = Math.floor(window.innerWidth);
-            let height = Math.floor(window.innerWidth * ratio);
+            let ratio: number = this.game.height / this.game.width;
+            let width: number = Math.floor(window.innerWidth);
+            let height:number = Math.floor(window.innerWidth * ratio);
 
             if (width % 2 === 1) {
                 width--;
@@ -73,7 +80,7 @@ class Display {
 
             this.resize(width, height);
 
-        } else if (this.width !== this.game.resolution.x) {
+        } else if (this.game.width !== this.game.resolution.x) {
             this.resize(
                 this.game.resolution.x,
                 this.game.resolution.y
@@ -81,7 +88,7 @@ class Display {
         }
     }
 
-    resize(width, height) {
+    resize(width: number, height: number) {
         this.ctx.canvas.width = width;
         this.ctx.canvas.height = height;
         this.ctx.imageSmoothingEnabled = false;
@@ -93,7 +100,7 @@ class Display {
         this.game.resize(width, height);
     }
 
-    register() {
+    register(): object {
         return {
             resize: e => this.triggerResize()
         };
