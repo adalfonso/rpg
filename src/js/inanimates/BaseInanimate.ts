3a42ee6fc78f60efefde0fe6c64e85c052e8e6bf
Anthony Dalfonso <anthony@anthonydalfonso.com>
2019-06-07 00:28:50 -0400

    Refactor: convert inanimates/BaseInanimate to typescript

--- src/js/inanimates/BaseInanimate.ts
+++ src/js/inanimates/BaseInanimate.ts
@@ -2,16 +2,20 @@ import config from '../config';
 import Vector from '../Vector';
 
 export default class BaseInanimate {
-    constructor(pos, size) {
+
+    public pos: Vector;
+    public size: Vector;
+
+    constructor(pos?: Vector, size?: Vector) {
         this.pos = pos || new Vector(0, 0);
         this.size = size || new Vector(0, 0);
     }
 
-    update(dt, player) {
+    update(dt: number) {
 
     }
 
-    draw(ctx) {
+    draw(ctx: CanvasRenderingContext2D) {
         if (config.debug) {
             ctx.strokeStyle = "#F00";
             ctx.strokeRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
