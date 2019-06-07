0ced5b6a67c9d7787c9bc6dd653b96ae089fe72c
Anthony Dalfonso <anthony@anthonydalfonso.com>
2019-06-06 22:07:17 -0400

    Refactor: clean up mount point

--- src/js/app.ts
+++ src/js/app.ts
@@ -2,22 +2,20 @@ import Game from './Game';
 import Display from './Display';
 import InputHandler from  './InputHandler';
 
-export let handler = new InputHandler();
+const GAME_WIDTH: number = 1280;
+const GAME_HEIGHT: number = 720;
+
+export let handler: InputHandler = new InputHandler();
 
 let canvas = <HTMLCanvasElement> document.getElementById('game');
-
-const GAME_WIDTH = 1280;
-const GAME_HEIGHT = 720;
-
-let game = new Game(GAME_WIDTH, GAME_HEIGHT);
-let display = new Display(canvas, game);
+let game: Game = new Game(GAME_WIDTH, GAME_HEIGHT);
+let display: Display = new Display(canvas, game);
+let lastTime: number = 0;
 
 game.start();
 
-let lastTime: number = 0;
-
 function frame(timestamp: number) {
-    let dt = timestamp - lastTime;
+    let dt: number = timestamp - lastTime;
     lastTime = timestamp;
 
     game.update(dt);
