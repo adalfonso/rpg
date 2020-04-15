4d7f497acf7b50660de3dd978d6c08844935485b
Anthony Dalfonso <anthonyjdalfonso@gmail.com>
2020-04-15 18:57:44 -0400

    Refactor: Clean up Inanimate class

--- src/ts/inanimates/Inanimate.ts
+++ src/ts/inanimates/Inanimate.ts
@@ -1,17 +1,48 @@
 import Vector from "@/Vector";
 import config from "@/config";
 
+/**
+ * Inanimates are entities that interact with other entitys in the game, but
+ * are stationary, and often invisible. e.g. clipping boundaries or portals.
+ */
 abstract class Inanimate {
+  /**
+   * Position of the entity
+   *
+   * @prop {Vector} pos
+   */
   public pos: Vector;
+
+  /**
+   * Size of the entity
+   *
+   * @prop {Vector} size
+   */
   public size: Vector;
 
+  /**
+   * Create and inanimate-based instance
+   *
+   * @param {Vector} pos  Position of the entity
+   * @param {Vector} size Size of the entity
+   */
   constructor(pos?: Vector, size?: Vector) {
-    this.pos = pos || new Vector(0, 0);
-    this.size = size || new Vector(0, 0);
+    this.pos = pos ?? new Vector(0, 0);
+    this.size = size ?? new Vector(0, 0);
   }
 
+  /**
+   * Update the Inanimate
+   *
+   * @param {number} dt Delta time
+   */
   update(dt: number) {}
 
+  /**
+   * Draw the Inanimate
+   *
+   * @param {CanvasRenderingContext2D} ctx Render context
+   */
   draw(ctx: CanvasRenderingContext2D) {
     if (config.debug) {
       ctx.strokeStyle = "#F00";
